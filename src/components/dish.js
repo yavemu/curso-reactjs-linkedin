57d9dd564f9f39c77eaf35cc265852df50571ee4
Yamid V.M <yavemu@gmail.com>
2019-04-23 11:52:41 -0500

    Importando Styles

--- src/components/dish.js
+++ src/components/dish.js
@@ -1,9 +1,19 @@
 import React, { Component, Fragment } from 'react';
 
+export class Ingredient extends Component {
+    render() {
+        return <Fragment>
+            <h4>Primer Ingrediente</h4>
+            <h4>Segundo Ingrediente</h4>
+        </Fragment>
+    }
+}
+
 class Dish extends Component {
     render() {
         return <Fragment>
             <h1>Un Platillo</h1>
+            <Ingredient/>
         </Fragment>
     }
 }
