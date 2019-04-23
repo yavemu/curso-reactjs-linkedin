d19b4533c5720afdfcd7a36abed0408c7f255a56
Yamid V.M <yavemu@gmail.com>
2019-04-23 11:05:17 -0500

    Importando componentes

--- src/components/dish.js
+++ src/components/dish.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Dish extends Component {
     render() {
         return <div>
-            <h1>Platillos</h1>
+            <h1>Un Platillo</h1>
         </div>
     }
 }
