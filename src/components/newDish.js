181f45b8b3394661f854a254b7e088818c1311f6
Yamid V.M <yavemu@gmail.com>
2019-04-23 15:05:26 -0500

    Referencia a un valor

--- src/components/newDish.js
+++ src/components/newDish.js
@@ -5,32 +5,36 @@ import AddIcon from "@material-ui/icons/Add";
 
 class NewDish extends Component {
 
-  addDish = (e) => {
-    console.log("Agregar platillo");
-    e.preventDefault();
-      console.log('imprimir this usando arrowFunction', this);
-  }
+    newDish = React.createRef();
 
-  render() {
-    return (
-      <form autoComplete="off" onSubmit={this.addDish}>
-        <TextField
-          label="Platillo..."
-          type="text"
-          margin="normal"
-          variant="outlined"
-        />
-        <Fab
-          color="primary"
-          size="medium"
-          className="dish-form-icon"
-          onClick={this.addDish}
-        >
-          <AddIcon />
-        </Fab>
-      </form>
-    );
-  }
+    addDish = (e) => {
+        console.log("Agregar platillo");
+        e.preventDefault();
+        console.log("Imprimiendo referencia [this.newDish.value] ", this.newDish.value);
+        
+    }
+
+    render() {
+        return (
+        <form autoComplete="off" onSubmit={this.addDish}>
+            <TextField
+                label="Platillo..."
+                type="text"
+                margin="normal"
+                variant="outlined"
+                inputRef={e => (this.newDish = e)}
+            />
+            <Fab
+                color="primary"
+                size="medium"
+                className="dish-form-icon"
+                onClick={this.addDish}
+            >
+            <AddIcon />
+            </Fab>
+        </form>
+        );
+    }
 }
 
 export default NewDish;
