b6d871a46216fa82b21725d6730bcb84fca0461a
Yamid V.M <yavemu@gmail.com>
2019-04-23 10:38:09 -0500

    Estructura del proyecto + Primeros componentes

---
+++ src/components/dish.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Dish extends Component {
+    render() {
+        return <div>
+            <h1>Platillos</h1>
+        </div>
+    }
+}
+
+export default Dish;
